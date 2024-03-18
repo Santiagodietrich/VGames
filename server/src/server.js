56fbe5b51c05ffec0bc2f0a11814352120652b47
santiagodietrich <santydietrich@gmail.com>
2024-03-18 18:03:22 -0300

    actualizacion nueva

--- server/src/server.js
+++ server/src/server.js
@@ -11,4 +11,18 @@ server.use(cors());
 
 server.use(router);
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+ });
+
 module.exports = server;
